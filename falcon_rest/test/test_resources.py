20fcdc904a47919ad26f93e39f05b11692c37ede
lng1982 <lng1982@gmail.com>
2025-02-28 13:04:23 +0900

    테스트 코드 오류 수정

--- falcon_rest/test/test_resources.py
+++ falcon_rest/test/test_resources.py
@@ -1,8 +1,8 @@
 import json
 
-import falcon_rest
+import falcon
 import pytest
-from falcon_rest import testing
+from falcon import testing
 
 from falcon_rest.resources import TodoListResource, TodoResource
 
@@ -10,7 +10,7 @@ from falcon_rest.resources import TodoListResource, TodoResource
 # 테스트 케이스 실행될 때마다 새로운 데이터 초기화
 @pytest.fixture
 def client():
-    app = falcon_rest.App()
+    app = falcon.App()
     app.add_route('/todos', TodoListResource())
     app.add_route('/todos/{todo_id}', TodoResource())
     return testing.TestClient(app)
@@ -18,7 +18,7 @@ def client():
 
 def test_get_todos(client):
     response = client.simulate_get('/todos')
-    assert response.status == falcon_rest.HTTP_200
+    assert response.status == falcon.HTTP_200
     data = json.loads(response.text)
     assert isinstance(data, list)
     assert len(data) > 0
@@ -27,7 +27,7 @@ def test_get_todos(client):
 def test_create_todo(client):
     new_todo = {"title": "Falcon 테스트", "done": False}
     response = client.simulate_post('/todos', json=new_todo)
-    assert response.status == falcon_rest.HTTP_201
+    assert response.status == falcon.HTTP_201
     data = json.loads(response.text)
     assert data["title"] == new_todo["title"]
     assert "id" in data
@@ -35,7 +35,7 @@ def test_create_todo(client):
 
 def test_get_specific_todo(client):
     response = client.simulate_get('/todos/1')
-    assert response.status == falcon_rest.HTTP_200
+    assert response.status == falcon.HTTP_200
     data = json.loads(response.text)
     assert data["id"] == 1
 
@@ -43,7 +43,7 @@ def test_get_specific_todo(client):
 def test_update_todo(client):
     update_data = {"title": "update title", "done": True}
     response = client.simulate_put('/todos/1', json=update_data)
-    assert response.status == falcon_rest.HTTP_200
+    assert response.status == falcon.HTTP_200
     data = json.loads(response.text)
     assert data["title"] == "update title"
     assert data["done"] is True
@@ -51,6 +51,6 @@ def test_update_todo(client):
 
 def test_delete_todo(client):
     response = client.simulate_delete('/todos/1')
-    assert response.status == falcon_rest.HTTP_200
+    assert response.status == falcon.HTTP_200
     response = client.simulate_get('/todos/1')
-    assert response.status == falcon_rest.HTTP_404
+    assert response.status == falcon.HTTP_404
